5eaee119ed0b06c2d7cf4938bb805fb784fc863e
darek <235607@edu.p.lodz.pl>
2025-03-31 08:37:23 +0200

    cwiczenie 1 mod

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ from csv import writer
 'Stworzenie funkcji'
 
 'Pozioma powierzchnia'
-def generate_horizontal_plane(szerokosc, dlugosc, num_points):
+def utworz_powierzchnie_pozioma(szerokosc, dlugosc, num_points):
     x = np.random.uniform(-szerokosc / 2, szerokosc / 2, num_points)
     y = np.random.uniform(-dlugosc / 2, dlugosc / 2, num_points)
     z = np.zeros(num_points)
     return np.column_stack((x, y, z))
 
 'Pionowa powierzchnia'
-def generate_vertical_plane(szerokosc, wysokosc, num_points):
+def utworz_powierzchnie_pionowa(szerokosc, wysokosc, num_points):
     x = np.random.uniform(-szerokosc / 2, szerokosc / 2, num_points)
     y = np.zeros(num_points)
     z = np.random.uniform(0, wysokosc, num_points)
     return np.column_stack((x, y, z))
 
 'Cylinder'
-def generate_cylindrical_surface(promien, wysokosc, num_points):
+def utworz_cylinder(promien, wysokosc, num_points):
     theta = np.random.uniform(0, 2 * np.pi, num_points)
     x = promien * np.cos(theta)
     y = promien * np.sin(theta)
@@ -36,12 +36,12 @@ def save_xyz(filename, points):
 num_points = 1000
 
 'Wywolanie funkcji i stworzenie chmury punktow 3 obiektow'
-horizontal_plane = generate_horizontal_plane(100, 100, num_points)
-vertical_plane = generate_vertical_plane(200, 50, num_points)
-cylindrical_surface = generate_cylindrical_surface(20, 30, num_points)
+powierzchnia_pozioma = utworz_powierzchnie_pozioma(100, 100, num_points)
+powierzchnia_pionowa = utworz_powierzchnie_pionowa(200, 50, num_points)
+cylinder = utworz_cylinder(20, 30, num_points)
 
 'Łączenie wszystkich punktów'
-all_points = np.vstack((horizontal_plane, vertical_plane, cylindrical_surface))
+all_points = np.vstack((powierzchnia_pozioma, powierzchnia_pionowa, cylinder))
 
 'Zapis do jednego pliku'
 save_xyz("chmura_punktow.xyz", all_points)
